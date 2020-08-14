e9c5ec79f9bf2b626bf884246f1b45dab7e38036
DevmiRajapakshe <65821269+DevmiRajapakshe@users.noreply.github.com>
2020-08-14 18:48:55 +0530

    FrontEndHomePage

--- app/src/main/java/com/example/bon_apetit/FrontEndHome.java
+++ app/src/main/java/com/example/bon_apetit/FrontEndHome.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FrontEndHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cutomer_profile);
+        setContentView(R.layout.activity_fronthome);
     }
 }

61ee7d38074a59efd286f9a94267b67c71ac597f
Mithila Panagoda <mithilapanagoda@gmail.com>
2020-08-16 13:46:30 +0530

    added active orders table deisgn/ updated manifest updated strings.xml

---
+++ app/src/main/java/com/example/bon_apetit/orders_table.java
@@ -0,0 +1,14 @@
+package com.example.bon_apetit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class orders_table extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders_table);
+    }
+}

eec8384b7ba6bf4c8f4038b445d0205bf2746437
Minuli <minulilokuliyanage@gmail.com>
2020-09-22 12:51:04 +0530

    payment added

---
+++ app/src/main/java/com/example/Models/Payment.java
@@ -0,0 +1,57 @@
+package com.example.Models;
+
+public class Payment {
+Long maxid;
+String address,email,date,time,method;
+
+    public Payment() {
+    }
+
+    public Long getMaxid() {
+        return maxid;
+    }
+
+    public void setMaxid(Long maxid) {
+        this.maxid = maxid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
--- app/src/main/java/com/example/bon_apetit/mycart.java
+++ app/src/main/java/com/example/bon_apetit/mycart.java
@@ -2,13 +2,34 @@ package com.example.bon_apetit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mycart extends AppCompatActivity {
+
+    Button checkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mycart);
+
+        checkout=findViewById(R.id.btncheckout);
+
+
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(),payment_one.class);
+                startActivity(intent);
+
+
+
+            }
+        });
     }
+
+
 }
